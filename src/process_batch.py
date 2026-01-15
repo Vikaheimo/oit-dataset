ce8d84780ba2d2489be9a5c19d89f82e76f4af9a
Vili Ikäheimo <vili.vikaheim@gmail.com>
2026-01-15 15:07:09 +0900

    refactor: code into src folder

--- src/process_batch.py
+++ src/process_batch.py
@@ -2,11 +2,11 @@ import logging
 import sys
 import os
 from pathlib import Path
+import numpy as np
+import pandas as pd
 
 from predict import ImageData, VideoData, predict_video_or_image
 from utils import get_log_level_from_env
-import numpy as np
-import pandas as pd
 
 logger = logging.getLogger(__name__)
 
